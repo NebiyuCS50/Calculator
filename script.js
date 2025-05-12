054425ecc61147a5d574b305b8f929ece1c49f01
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-05-12 12:52:07 +0300

    feat: build calculator layout with centered container and styled buttons

---
+++ script.js
@@ -0,0 +1,20 @@
+const add = function(a, b){
+    return a+b;
+}
+
+const subtract = function (a,b){
+    return a-b;
+}
+
+const multiplication = function (a,b){
+    return a*b;
+}
+
+const division = function (a,b){
+    return a/b;
+}
+
+console.log(add(2,3));
+console.log(subtract(2,3));
+console.log(multiplication(2,3));
+console.log(division(2,3));
