ecfeaa44dddf3a0ddc21059b9ccb4b70fff37a63
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-05-13 13:28:12 +0300

    Add calculator functionality

--- script.js
+++ script.js
@@ -1,20 +1,85 @@
-const add = function(a, b){
-    return a+b;
+let result = "";
+function add(fNum, sNum){
+    result = parseInt(fNum) + parseInt(sNum);
+    return parseInt(result);
+}
+function sub(fNum, sNum){
+    result = parseInt(fNum) - parseInt(sNum);
+    return parseInt(result);
+}
+function mul(fNum, sNum){
+    result = parseInt(fNum) * parseInt(sNum);
+    return parseInt(result);
+}
+function divide(fNum, sNum){
+    result = parseInt(fNum) / parseInt(sNum);
+    return parseInt(result);
 }
 
-const subtract = function (a,b){
-    return a-b;
-}
 
-const multiplication = function (a,b){
-    return a*b;
-}
 
-const division = function (a,b){
-    return a/b;
-}
 
-console.log(add(2,3));
-console.log(subtract(2,3));
-console.log(multiplication(2,3));
-console.log(division(2,3));
+let firstNumber = "";
+let secondNumber = "";
+let isSecondNumber = false;
+const fNumbers = document.querySelectorAll(".num").forEach(button=>
+button.addEventListener('click', function(){
+    if(!isSecondNumber){
+        firstNumber = firstNumber + this.textContent;
+        console.log(`firstNumber: ${firstNumber}`)
+    }
+    else{
+        secondNumber = secondNumber + this.textContent
+        console.log(`secondNumber: ${secondNumber}`)
+    }
+}))
+
+let operator = "";
+const operators = document.querySelectorAll(".sign").forEach(button=>{
+    button.addEventListener('click', function(){
+        operator = this.textContent;
+        isSecondNumber = true;
+        console.log(operator)
+        secondNumber = "";
+    })
+})
+
+const equals = document.querySelector(".answer");
+equals.addEventListener('click', function(){
+    if(firstNumber && secondNumber && operator=="+"){
+        console.log(add(firstNumber, secondNumber));
+        firstNumber="";
+        secondNumber="";
+        isSecondNumber= false;
+    }
+    else if(firstNumber && secondNumber && operator=="-"){
+        console.log(sub(firstNumber, secondNumber));
+        firstNumber="";
+        secondNumber="";
+        isSecondNumber= false;
+    }
+    else if(firstNumber && secondNumber && operator=="X"){
+        console.log(mul(firstNumber, secondNumber));
+        firstNumber="";
+        secondNumber="";
+        isSecondNumber= false;
+    }
+    else if(firstNumber && secondNumber && operator=="/"){
+        if(secondNumber != '0'){
+        console.log(divide(firstNumber, secondNumber));
+        firstNumber="";
+        secondNumber="";
+        isSecondNumber= false;
+        }
+        else{
+            console.log("syntax error")
+        }
+    }
+    else{
+        console.log("syntax error");
+        firstNumber="";
+        secondNumber="";
+        isSecondNumber= false;
+    }
+})
+
