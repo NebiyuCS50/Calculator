c705fa63ffcfb31cb8ac6dcf2ea1d1fe4c33883f
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-05-17 12:29:16 +0300

    feat: implement the requirements in odin proj

--- script.js
+++ script.js
@@ -1,73 +1,101 @@
-function add(numFirst, numSecond){
-    result = parseInt(numFirst) + parseInt(numSecond)
-    return result;
+function add(numFirst, numSecond) {
+  result = parseFloat(numFirst) + parseFloat(numSecond);
+  return result.toFixed(7);
 }
-function subtract(numFirst, numSecond){
-    result = parseInt(numFirst) - parseInt(numSecond)
-    return result;
+function subtract(numFirst, numSecond) {
+  result = parseFloat(numFirst) - parseFloat(numSecond);
+  return result.toFixed(7);
 }
-function multiply(numFirst, numSecond){
-    result = parseInt(numFirst) * parseInt(numSecond)
-    return result;
+function multiply(numFirst, numSecond) {
+  result = parseFloat(numFirst) * parseFloat(numSecond);
+  return result.toFixed(7);
 }
-function divide(numFirst, numSecond){
-    result = parseInt(numFirst) / parseInt(numSecond)
-    return result;
-}
-
-function operate(num1, num2, opr){
-    switch(opr){
-        case '+': return add(num1, num2);
-        case '-': return subtract(num1, num2);
-        case '*': return multiply(num1, num2);
-        case '/': return divide(num1, num2);
-        default: return num2;
-    }
+function divide(numFirst, numSecond) {
+  result = parseFloat(numFirst) / parseFloat(numSecond);
+  return result.toFixed(7);
 }
 
-let isSecond = false
-let firstNumber = ""
-let secondNumber = ""
-const inpFirst = document.querySelector(".input")
-const inpSecond = document.querySelector(".input_2")
-const numbers = document.querySelectorAll(".num")
-const answer = document.querySelector('.answer')
-numbers.forEach(num => num.addEventListener('click', function(){
-    if(!isSecond){
-        firstNumber = firstNumber + this.textContent
-        console.log(firstNumber)
-        inpFirst.textContent = firstNumber
-    }
-    else{
-        secondNumber= secondNumber + this.textContent
+function operate(num1, num2, opr) {
+  switch (opr) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      return num2;
+  }
+}
+
+let isSecond = false;
+let firstNumber = "";
+let secondNumber = "";
+const inpFirst = document.querySelector(".input");
+const inpSecond = document.querySelector(".input_2");
+const numbers = document.querySelectorAll(".num");
+const answer = document.querySelector(".answer");
+const clear = document.querySelector(".clr");
+numbers.forEach((num) =>
+  num.addEventListener("click", function () {
+    if (!isSecond) {
+      if (firstNumber.length < 10) {
+        firstNumber = firstNumber + this.textContent;
+        console.log(firstNumber);
+        inpFirst.textContent = firstNumber;
+      }
+    } else {
+      if (secondNumber.length < 10) {
+        secondNumber = secondNumber + this.textContent;
         inpFirst.textContent = secondNumber;
+      }
     }
-}))
+  })
+);
 
 let operator = "";
-const operators = document.querySelectorAll(".sign")
-operators.forEach(opr => opr.addEventListener('click', function(){
-    if(secondNumber !== ""){
+const operators = document.querySelectorAll(".sign");
+operators.forEach((opr) =>
+  opr.addEventListener("click", function () {
+    if (secondNumber !== "") {
+      if (secondNumber == "0" && operator == "/") {
+        inpSecond.textContent = "Can not divide by zero";
+      } else {
         firstNumber = operate(firstNumber, secondNumber, operator);
         inpFirst.textContent = firstNumber;
         secondNumber = "";
+      }
     }
     operator = this.textContent;
-    inpFirst.textContent = operator
+    inpFirst.textContent = operator;
     isSecond = true;
-}))
+  })
+);
 
-answer.addEventListener('click', function(){
-    if(secondNumber !== ""){
-        result = operate(firstNumber, secondNumber, operator);
-        inpSecond.textContent = result;
-        firstNumber = result;
-        secondNumber = "";
-        operator = "";
-        isSecond = false;
+answer.addEventListener("click", function () {
+  if (secondNumber !== "") {
+    if (secondNumber == "0" && operator == "/") {
+      inpSecond.textContent = "Can not divide by zero";
+    } else {
+      result = operate(firstNumber, secondNumber, operator);
+      inpSecond.textContent = result;
+      firstNumber = "";
+      inpFirst.textContent = "";
+      secondNumber = "";
+      operator = "";
+      isSecond = false;
     }
-})
+  } else {
+    inpSecond.textContent = "Syntax Error";
+  }
+});
 
-
-
-
+clear.addEventListener("click", function () {
+  firstNumber = "";
+  inpFirst.textContent = "";
+  secondNumber = "";
+  inpSecond.textContent = "";
+  isSecond = false;
+});
